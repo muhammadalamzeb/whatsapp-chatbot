e6118c6647cf88ed15e73a24d4f6112178da4675
MuhammadAlamzeb <shayankhanmahar@gmail.com>
2023-09-26 00:41:06 +0500

    Header added

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Animation for header text
+/*
 const header = document.querySelector('header');
 header.style.opacity = '0';
 
 setTimeout(() => {
     header.style.transition = 'opacity 1s';
     header.style.opacity = '1';
-}, 500);
+}, 500);*/
